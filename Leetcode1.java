96e4834f39ea7cbbef67f5a2ae66af7cdae0996f
Joe <geekjoechan@gmail.com>
2019-10-21 21:03:13 +0800

    两数之和

---
+++ Leetcode1.java
@@ -0,0 +1,31 @@
+/**
+ * 两数之和
+ */
+public class Leetcode1 {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (target - nums[i] == nums[j]) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+    }
+
+    public int[] twoSumByMap(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int res = target - nums[i];
+            if (map.containsKey(res) && map.get(res) != i) {
+                return new int[] {i, map.get(res)};
+            }
+        }
+
+        return null;
+    }
+}
