a7ffa3eda9c00e57a3bc38004a78ceb4d1b1b694
Joe <geekjoechan@gmail.com>
2019-10-23 20:36:53 +0800

    Update Leetcode894.java

--- Leetcode894.java
+++ Leetcode894.java
@@ -1,1 +1,49 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
+/**
+ * 所有可能的满二叉树
+ */
+public class Leetcode894 {
+    Map<Integer, List<TreeNode>> memo = new HashMap<>();
+
+    public List<TreeNode> allPossibleFBT(int N) {
+        if (!memo.containsKey(N)) {
+            List<TreeNode> ans = new LinkedList<>();
+            if (N == 1) {
+                ans.add(new TreeNode(0));
+            } else if (N % 2 == 1) {
+                for (int x = 0; x < N; x++) {
+                    int y = N - 1 - x;
+                    for (TreeNode left : allPossibleFBT(x)) {
+                        for (TreeNode right: allPossibleFBT(y)) {
+                            TreeNode bns = new TreeNode(0);
+                            bns.left = left;
+                            bns.right = right;
+                            ans.add(bns);
+                        }
+                    }
+                }
+            }
+
+            memo.put(N, ans);
+        }
+        return memo.get(N);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Leetcode894().allPossibleFBT(7));
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
